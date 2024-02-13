1c48d9905e7895eb44eed67a294a213c506cddbc
Carlos <carlosoliveira1898@outlook.com>
2024-02-13 15:24:02 -0300

    Exercicios sobre construtores e sobrecarga em C#

---
+++ ConsoleApp1/ConsoleApp1/Produto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1 {
+    class Produto {
+
+        public string Nome;
+        public double Preco;           // Atributos
+        public int Quantidade;
+
+        // Sobrecarga com dois construtores, um com 3 argumentos e outro com 2 argumentos
+        public Produto() { //Construtor padrão
+            Quantidade = 0;
+        }
+        
+        public Produto(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;                                    // Construtor (atributo = construtor)
+            Quantidade = quantidade;
+        }
+        
+        /*public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 0; //Qualquer valor pode ser incluído aqui
+        }*/
+
+        public Produto(string nome, double preco) : this() { //Referenciar outro construtor em um contrutor, sendo possível referenciar dentro dos parênteses argumentos de outros construtores
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleApp1;
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preco: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco);   // Instanciação recebendo as variáveis auxiliares criadas anteriormente com dois argumentos
+
+            Produto p2 = new Produto {
+                Nome = "TV",
+                Preco = 500.0,
+                Quantidade = 2
+            };
+            
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionados ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
